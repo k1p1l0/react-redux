5409630c505943847fbc2218358f39c5e4ecb186
Kirill Kozak <s0ht@mail.ru>
2017-02-22 16:08:21 +0200

    Update the webpack.config

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default class Layout extends React.Component {
   render() {
-    return <div>It works! Yeah!</div>
+    return <div>It works! Yeah!???</div>
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 var debug = process.env.NODE_ENV !== "production";
 var webpack = require('webpack');
+let CopyWebpackPlugin = require('copy-webpack-plugin');
+let CleanWebpackPlugin = require('clean-webpack-plugin');
 var path = require('path');
 
 module.exports = {
   context: path.join(__dirname, "src"),
   devtool: debug ? "inline-sourcemap" : null,
-  entry: "./js/client.js",
+  entry: "./client.js",
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.json']
+  },
   module: {
     loaders: [
       {
@@ -16,16 +21,39 @@ module.exports = {
           presets: ['react', 'es2015', 'stage-0'],
           plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
         }
+      },
+      {
+          test: /\.json$/,
+          loader: 'json'
       }
     ]
   },
   output: {
-    path: __dirname + "/src/",
+    path: path.resolve(__dirname, "build"),
+    publicPath: '/public/',
     filename: "client.min.js"
   },
-  plugins: debug ? [] : [
+  plugins: debug ? [
+      new CopyWebpackPlugin([
+        { from: './../public', to: './../build' },
+      ])
+    ] : [
+    new webpack.DefinePlugin({
+      'process.env': {
+          NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
+    new CleanWebpackPlugin(['build'], {
+      root: __dirname,
+      verbose: true, 
+      dry: false,
+    }),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new CopyWebpackPlugin([
+      { from: './../public', to: './../build' },
+    ])
   ],
 };
